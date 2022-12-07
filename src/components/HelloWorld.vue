6480c382d215bfd228aeadead6a58a90181000cd
Mihray <2636853047@qq.com>
2022-12-07 11:48:15 +0800

    fit:axios发送请求首次成功

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,65 @@
 <template>
-  <div class="hello">
+  <div class="hello" @click="boxclick">
     HelloWorld
+    <span>{{info}}</span>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+// const myAxios = axios.create({
+//     baseURL:'http://111.229.176.245:3000/mtapi/mihray/',
+// });
+
+// const getReq = {
+//     getName: 'getName',
+//     sayHello: 'sayHello',
+//     getWeather: 'randomWeather'
+// }
+// const postReq = {
+//     login: 'login'
+// }
+
 export default {
   name: 'HelloWorld',
   props: {
-   
+  },
+  data(){
+    return{
+      info:null,
+      form:{
+                userName:'',
+                password:'',
+            },
+    }
+  },
+  methods:{
+    boxclick(){
+      axios.get('/mtapi/mihray/randomWeather')
+      .then(res=>{
+              console.log(res.data)})
+        .catch((err)=>{console.log(err);}),
+
+      axios.get('/mtapi/mihray/sayHello')
+      .then(res=>{
+              console.log(res.data)})
+        .catch((err)=>{console.log(err);}),
+          axios.post('/mtapi/mihray/login')
+          .then(res=>{
+              console.log(res.data)
+          })
+          .catch((err)=>{console.log(err);});
+        }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.hello{
+  width:200px;
+  height:200px;
+  background-color: blue;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import axios from 'axios'
 
 createApp(App).mount('#app')
+// App.prototype.$axios=axios //挂载到原型，可以全局使用
+
+
+
